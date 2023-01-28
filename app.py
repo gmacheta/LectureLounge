adaf59a2d7c41d71a854288de7a32e71a53550c6
jcolon030 <99977621+jcolon030@users.noreply.github.com>
2023-01-28 17:21:19 -0500

    update

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from website import create_app
+from flask import Flask, render_template
 
 app = create_app()
+
+@app.route('/sign-up')
+def sign_up():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
