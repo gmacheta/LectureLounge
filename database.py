cb1bb2b68346ad5695be3f93b675a5f3d0a151d2
jcolon030 <99977621+jcolon030@users.noreply.github.com>
2023-01-28 13:27:46 -0500

    class Database

--- database.py
+++ database.py
@@ -1,13 +1,21 @@
 from pymongo import MongoClient
 
-def get_database():
-    
-    CONNECTION_STRING = "mongodb+srv://<user>:<SpartaHack8>@spartahack8.jdqhm8z.mongodb.net/?retryWrites=true&w=majority"
-    
-    client = MongoClient(CONNECTION_STRING)
-    
-    return client['LectureLounge']
+class DB_Client:
 
-dbname = get_database()
+    def __init__(self):
+        
+        try:
+    
+            self.CONNECTION_STRING = "mongodb+srv://<user>:<SpartaHack8>@spartahack8.jdqhm8z.mongodb.net/?retryWrites=true&w=majority"
+            
+            self.client = MongoClient(self.CONNECTION_STRING)
+            
+            print("It connected")
+        
+        except:
+            
+            print("Did not connect")
+
+dbname = DB_Client()
 
 print(dbname)
