5540524f3558014af3973bc201cb1c03d593295c
yadito <epardo1742@proton.me>
2023-01-28 17:27:00 -0500

    h
    chqn

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,45 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import pymongo
 from os import path
 from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = 'db'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
+    app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init__app(app)
 
     from .views import views
     from .auth import auth
 
+    create_datbase(app)
+
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User
+
+
+    create_datbase(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    #create session
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+        
     return app
+
+
+def create_datbase(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print("created")
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,34 @@
-from flask import Blueprint, render_template, redirect, url_for
-
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from .import db
+from models import User
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    email = request.form.get('email')
+    username = request.form.get('username')
+    password = request.form.get('password')
     return render_template('login.html')
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password')
+        password2 = request.form.get('password2')
+
+       email_exists = User.query.filter_by(email=email)
+       username_exists = User.query.filter_by(username=username)
+.first()
+    if email_exists:
+        flash("Email is already in use")
+
     return render_template('signup.html')
 
 @auth.route('/logout')
 def logout():
     return redirect(url_for('views.home'))
+
+@auth.route
