59e77b48abef7aac372cff60df85ce101e672a0c
yadito <epardo1742@proton.me>
2023-01-28 19:36:55 -0500

    pass changes

--- database.py
+++ database.py
@@ -1,56 +1,44 @@
 from pymongo import MongoClient
+import bcrypt
 
 class DB_Client:
-
     def __init__(self):
-        
         try:
-    
             self.CONNECTION_STRING = "mongodb+srv://User:SpartaHack8@spartahack8.jdqhm8z.mongodb.net/?retryWrites=true&w=majority"
-            
             self.client = MongoClient(self.CONNECTION_STRING)
-            
             print("It connected")
-        
         except:
-            
             print("Did not connect")
-            
+
     def get_users_collection(self):
-        
         return self.client["LectureLounge"]["users"]
-    
+
     def get_categories_collection(self):
-        
         return self.client["LectureLounge"]["categories"]
 
-    def new_user(self):
-        
-    
+    def create_user(self, email, password,{school}{date}{username}):
+        collection = self.get_users_collection()
+
+        # Hash the password
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+        # Create the user document
+        new_user = {
+            "_id": email,
+            "password": hashed_password
+        }
+
+        # Insert the user into the collection
+        collection.insert_one(new_user)
+        print("user created")
+
     def list_users(self):
-        
         db_pointer = self.get_users_collection()
-        
         for item in db_pointer.find():
             print(item)
-            
-    def create_user(self, name, date, school, categories,):
-        
-        collection = self.get_users_collection()
-        
-        new_user = {
-            "_id": name,
-            "date_created":date,
-            "school":school,
-            "categories": categories
-        }
-        
-        collection.insert_one(new_user)
-    
+
     def list_user_categories(self, username):
-        
         user = self.get_users_collection().find_one({"_id":username})
-        
         return user["categories"]
        
         
@@ -59,11 +47,7 @@ class DB_Client:
 
 dbname = DB_Client()
 
-<<<<<<< HEAD
-dbname.create_user("tim", "10-20-12", "MSU", ["art", "history"])
-=======
-#dbname.create_user("Jay", "10-20-12", "MSU", ["art", "history"])
 
-print(dbname.list_user_categories("Jay"))
->>>>>>> c86d886d5e3f038859eb1780c3ebaccdbd58d91a
+
+
 
